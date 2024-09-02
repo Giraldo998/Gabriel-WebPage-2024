54966d07b40622ce561b4b25e60a9743a29a0316
Sebastian Giraldo <sebasvelez293@hotmail.com>
2024-09-02 01:44:23 -0500

    refactorizacion de data

--- src/assets/data/about/aboutData.js
+++ src/assets/data/about/aboutData.js
@@ -2,16 +2,19 @@ import { coverAbout } from "../../img";
 
 export const aboutData = [
    {
+      id: 1,
       profile: 'Docente',
       description: '"Regresar a otros lo que en mi momento tuve la oportunidad de aprender" Un recorrido por los procesos de formación en los cuales he tenido la oportunidad de participar.',
       picture: coverAbout
    },
    {
+      id: 2,
       profile: 'Artista',
       description: 'La vida está llena de oportunidades, oportunidades de conocer lugares magníficos y seres humanos excepcional y la mejor compañía para esas oportunidades es la música, vivir la música.',
       picture: coverAbout
    },
    {
+      id: 3,
       profile: 'Gestor investigador',
       description: 'A través de diferentes instituciones he podido buscar las necesidades del sector musical y han sido los propios artistas quienes han planteado a través de estos programas las posibles soluciones. Algunas de las soluciones se han convertido en proyectos que hemos seguido para encontrar los recursos humanos y financieros para su realización.',
       picture: coverAbout
--- src/assets/data/gallery/galleryData.js
+++ src/assets/data/gallery/galleryData.js
@@ -2,31 +2,37 @@ import {oroMolido, funza, gradosFuba, mexico, planMusical, peru} from '../../img
 
 export const galleryData = [
 	{
+		id: 1,
 		galleryName: 'Oro molido',
 		albumUrl: 'https://photos.app.goo.gl/CBFn2uk5EdP2wjR49',
 		backgroundCard: oroMolido,
 	},
 	{
+		id: 2,
 		galleryName: 'Viaje a Perú',
 		albumUrl: 'https://photos.app.goo.gl/gVZFwShF8j4qAcYE7',
 		backgroundCard: peru,
 	},
 	{
+		id: 3,
 		galleryName: 'Plan de desarrollo musical PAP Cartama',
 		albumUrl: 'https://photos.app.goo.gl/M1u8V8Y2deGJJdtFA',
 		backgroundCard: planMusical,
 	},
 	{
+		id: 4,
 		galleryName: 'Grados FUBA',
 		albumUrl: 'https://photos.app.goo.gl/ast6zXhSQpZHeH2P8',
 		backgroundCard: gradosFuba,
 	},
 	{
+		id: 5,
 		galleryName: 'Viaje a México',
 		albumUrl: 'https://photos.app.goo.gl/54tNk75ENrxFxdXW7',
 		backgroundCard: mexico,
 	},
 	{
+		id: 6,
 		galleryName: 'Viaje a Funza',
 		albumUrl: 'https://photos.app.goo.gl/SVctQvm36R7ZHCKj8',
 		backgroundCard: funza,
